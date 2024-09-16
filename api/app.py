a41d18a3e84cb512967f416043a4776f45113199
Ernest Matshabe <matshabecapital@gmail.com>
2024-09-16 16:41:45 +0200

    added navbar | user's can now log out

--- api/app.py
+++ api/app.py
@@ -1,12 +1,32 @@
-from flask import Flask, jsonify, request, redirect, session, render_template
+from flask import Flask, jsonify, request, url_for, session, render_template, redirect
 from flask_cors import CORS
+from authlib.integrations.flask_client import OAuth
 import json
+import os
 
 app = Flask(__name__, template_folder='../client')
 CORS(app)
 
+app.secret_key = os.urandom(20)
+oauth = OAuth(app)
+oauth.init_app(app)
+google = oauth.register(
+    name='login1',
+    client_id='154338881871-h2b3b3f9cnd16k35gpqqtmssk3umptb0.apps.googleusercontent.com',
+    client_secret='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',
+    access_token_url='https://oauth2.googleapis.com/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    authorize_params=None,
+    access_token_params=None,
+    refresh_token_url=None,
+    redirect_uri='http://localhost:5000/login-google-form',
+    client_kwargs={'scope': 'openid profile email'},
+)
+
 
 db = {}
+
 
 @app.route("/")
 def root():
@@ -16,6 +36,28 @@ def root():
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+
+@app.route("/login-google")
+def gAuth():
+    return oauth.login1.authorize_redirect(redirect_uri=url_for('gAuthForm', _external=True))
+
+
+@app.route("/login-google-form")
+def gAuthForm():
+    token = google.authorize_access_token()
+    session['name'] = token
+    email = token["userinfo"]["email"]
+
+    if email not in db:
+        form = {
+            "name": token["userinfo"]["given_name"],
+            "surname": token["userinfo"]["family_name"],
+            "email": email,
+            "password": token["access_token"]
+        }
+        db[email] = form
+    return redirect(url_for('home'))
 
 
 @app.route("/login")
@@ -31,6 +73,7 @@ def loginForm():
         requestData = json.loads(requestData)
         if requestData["email"] in db: 
             if requestData["password"] == db[requestData["email"]]["password"]:
+                session["name"] = requestData["email"]
                 return jsonify({"message":"you're in", "status":"successful"})
             else:
                 return jsonify({"message":"login failed", "status":"error"})
@@ -62,7 +105,7 @@ def registrationForm():
 
 @app.route("/logout", methods=["GET"])
 def logout():
-    return "logout process"
+    return redirect(url_for("/"))
 
 
 @app.route("/forgot-password", methods=["POST"])
