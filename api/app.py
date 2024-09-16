e605e5fba25be9c0415bc687e4933565b411c6c0
Ernest Matshabe <matshabecapital@gmail.com>
2024-09-16 12:10:58 +0200

    change registration button type to 'click' | server rejects registration if email already exist and pops up an alert box if user successfully register

--- api/app.py
+++ api/app.py
@@ -6,11 +6,7 @@ app = Flask(__name__, template_folder='../client')
 CORS(app)
 
 
-database = {
-    "admin":12345,
-    "ematshabe023@student.wethinkcode.co.za":"12345"
-}
-
+db = {}
 
 @app.route("/")
 def root():
@@ -26,21 +22,22 @@ def home():
 def login():
     return render_template("login-page.html")
 
+
 @app.route("/login-form", methods=["POST"])
 def loginForm():
     requestData = request.get_data().decode('utf8').replace("'", '"')
     
     if request.method == "POST":
         requestData = json.loads(requestData)
-        if requestData["email"] in database: 
-            if requestData["password"] == database[requestData["email"]]:
+        if requestData["email"] in db: 
+            if requestData["password"] == db[requestData["email"]]["password"]:
                 return jsonify({"message":"you're in", "status":"successful"})
             else:
                 return jsonify({"message":"login failed", "status":"error"})
         else:
             return jsonify({"message":"email doesn't exist", "status":"error"})
 
-    return jsonify({"response": "done"})
+    return jsonify({"message":"none", "status":"error"})
 
 
 @app.route("/registration", methods=["GET"])
@@ -50,18 +47,17 @@ def registration():
 
 @app.route("/registration-form", methods=["POST"])
 def registrationForm():
-    
     if request.method == "POST":
         requestData = request.get_data().decode('utf8').replace("'", '"') 
-        print(requestData)
-        # requestData = json.loads(requestData)
-        # if requestData["email"] not in database: 
-        #     jsonify({"message":"Registration complete", "status":"successful"})
-        # else:
-        #     return jsonify({"message":"User already exist", "status":"error"})
-        return jsonify({"response": "done"})
+        requestData = json.loads(requestData)
 
-    return jsonify({"response": "done"})
+        if requestData["email"] not in db:
+            db[requestData["email"]] = requestData
+            return jsonify({"message":"registration complete", "status":"successful"})
+        else:
+            return jsonify({"message":"user already exist", "status":"error"})        
+
+    return jsonify({"message":"none", "status":"error"})
 
 
 @app.route("/logout", methods=["GET"])
